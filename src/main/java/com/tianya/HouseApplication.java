2e8c0abf51de7ea64d05cfc48bb06b72c6a3dcd4
wenbochang888 <727987105@qq.com>
2019-09-17 22:00:00 +0800

    B3

--- src/main/java/com/tianya/HouseApplication.java
+++ src/main/java/com/tianya/HouseApplication.java
@@ -9,6 +9,6 @@ public class HouseApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(HouseApplication.class, args);
-		System.out.println("ccc");
+		System.out.println("b1");
 	}
 }
